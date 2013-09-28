578d7945fc00b25af30c0e0624e1c552401f4f4c
appost <luigi.cirillo41@yahoo.co.uk>
2013-09-28 13:00:32 +0200

    generated project

---
+++ oscar_openerp/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'oscar_openerp.views.home', name='home'),
+    # url(r'^oscar_openerp/', include('oscar_openerp.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
