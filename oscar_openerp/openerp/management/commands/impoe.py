46119840d91a8ccc8e2c0d6e536c7200e1295767
appost <luigi.cirillo41@yahoo.co.uk>
2013-10-04 16:21:33 +0100

    working attemp to mapping datas

--- oscar_openerp/openerp/management/commands/impoe.py
+++ oscar_openerp/openerp/management/commands/impoe.py
@@ -8,11 +8,23 @@ from oscar_openerp import settings
 from django.db import models
 from oscar.apps.catalogue.models import ProductClass
 from oscar_openerp.openerp import mapping 
+import oscar
 
 def distinct(seq):
     seen = set()
     seen_add = seen.add
     return [ x for x in seq if x not in seen and not seen_add(x)]
+
+
+def distinct_dict(seq):
+    seen = set()
+    new_seq = []
+    for d in seq:
+        t = tuple(sorted(d.items()))
+        if t not in seen:
+            seen.add(t)
+            new_seq.append(d)
+    return new_seq
 
 
 class Command(NoArgsCommand):
@@ -36,17 +48,20 @@ class Command(NoArgsCommand):
         for i in range(0, len(oecate)):
             productclass = ProductClass(id=i,name=oecate[i],slug=oecate[i])
             productclass.save()
-        import ipdb; ipdb.set_trace()
         for oe_mod_name in mapping.mapping:   
             oe_mod_val = Client.read(oe_mod_name,[],mapping.mapping[oe_mod_name][1].keys())
             if 'id' not in mapping.mapping[oe_mod_name][1].keys():
                 for dict in oe_mod_val:
                     del dict['id']
+                oe_mod_val = distinct_dict(oe_mod_val)
+            oscar_mod = eval(mapping.mapping[oe_mod_name][0])
             for i in range(0, len(oe_mod_val)):
-                oscar_mod_obj = mapping.mapping[oe_mod_name][1](id = i)
+                oscar_mod_obj = oscar_mod(id = i)
                 for oe_field in oe_mod_val[i].keys():
-                    oeattr(getattr(oe_mod_val,oe_field))
-                    setattr(oscar_mod_obj, mapping.mapping[oe_mod_name][1][oe_field], )
+                    oeattr=oe_mod_val[i][oe_field]
+                    for attr in mapping.mapping[oe_mod_name][1][oe_field]:
+                        setattr(oscar_mod_obj, attr, oeattr)
+                #import ipdb; ipdb.set_trace()
                 oscar_mod_obj.save()
         
         
